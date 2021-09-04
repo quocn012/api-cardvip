f0982d34a412f94396111438bec8f783960a23cb
quocn012 <62655234+quocn012@users.noreply.github.com>
2021-09-04 16:45:09 +0700

    Update api-chuyen-tien-cardvip.php

--- api-chuyen-tien-cardvip.php
+++ api-chuyen-tien-cardvip.php
@@ -1,1 +1,52 @@
-
+<?php
+/**
+ * API ĐƯỢC VIẾT BỞI CHÂU CHÍ QUỐC
+ * THỂ HIỆN MÌNH KHÔNG PHẢI SÚC VẬT BẰNG CÁCH KO XÓA CMT NẦY
+ * ZALO 01684853992
+ * FB : https://www.facebook.com/chauchi.quoc.5
+ * THẰNG NÀO ĐEM BÁN TAO ĐÁ THỤC MỖM :v
+*/
+header('Content-Type: application/json');
+// đoạn nầy login vào
+$username = ''; // tài khoản
+$password = ''; // mật khẩu
+$loginUrl = 'https://cardvip.vn/dang-nhap';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $loginUrl);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'email='.$username.'&password='.$password);
+curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
+curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_NOBODY, false);
+$content = curl_exec($ch);
+$headers = curl_getinfo($ch, CURLINFO_HEADER_OUT);
+curl_close($ch);
+$json = json_decode($content);
+if($json->status == true){
+    // đoạn nầy thực hiện chuyễn tiền
+    $sdt = ""; // tên tài khoảng
+    $sotien = ""; // số tiền cần chuyễn
+    $noidung = ""; // nội dung cần chuyễn
+    $matkhaucap2 = ""; // mật khẩu cấp 2
+    $url_chuyen_tien = 'https://cardvip.vn/gd-chuyen-tien';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url_chuyen_tien);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
+    curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, 'PhoneNumber='.$sdt.'&ipTotalAmountMove='.$sotien.'&NoteMoveAmountAccount='.$noidung.'&PasswordAdvancedMoveAmount='.$matkhaucap2);
+    $content = curl_exec($ch);
+    curl_close($ch);
+    $json = json_decode($content);
+    echo $json->msg;
+}else{
+    echo $json->msg;
+}
+?>
